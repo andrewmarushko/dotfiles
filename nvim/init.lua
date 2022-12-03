21872cb8ac51040bd2e589b9ed39a6ede933e9d2
Andrew Marushko <andrewmarushko@gmail.com>
2022-12-03 21:32:31 +0100

    Install gruvebox and packer

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,12 @@ require('base')
 require('highlight')
 require('macos')
 require('maps')
+require('setup-plugins')
+require('grovebox')
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+
+if is_mac then
+  require('macos')
+end
