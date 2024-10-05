69aa92fdf44774833217be4d6a6485a8c81ccec5
Andrew Marushko <andrew@inkan.news>
2024-10-05 16:58:04 +0200

    feat: basic dev workflow 🎉

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,26 @@
+local opts = { noremap = true, silent = true }
+
+
+-- Keep cursor centered when scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+
+-- Move selected line / block of text in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
+
+-- Fast saving
+-- vim.keymap.set("n", "<leader>w", ":write!<CR>", { silent = true, desc = "Save file" })
+-- vim.keymap.set("n", "<leader>q", ":q!<CR>", opts)
+
+-- Remap for dealing with visual line wraps
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+
+-- better indenting
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- paste over currently selected text without yanking it
+vim.keymap.set("v", "p", '"_dp')
+vim.keymap.set("v", "P", '"_dP')
